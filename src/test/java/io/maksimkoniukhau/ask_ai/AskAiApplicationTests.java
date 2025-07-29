665beb634ec15013b5f4ad5a62ef65f38a1cd4e1
Maksim Koniukhau <koniukhau.maksim@gmail.com>
2025-07-29 23:24:57 +0200

    Init project

---
+++ src/test/java/io/maksimkoniukhau/ask_ai/AskAiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.maksimkoniukhau.ask_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AskAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
